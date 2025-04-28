3c9d36273ebc6374962c735a943c0aa91f1463c7
Benjay420 <120934234+Benjay420@users.noreply.github.com>
2025-04-28 10:04:05 +0200

    Tile.cs – moves downward and destroys on click or off-screenTile.cs – moves downward and destroys on click or off-screen
    Tilføjet Tile.cs

---
+++ My project (2)/Assets/Scripts/Tile.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    [Tooltip("Fall speed in units per second")]
+    public float speed = 5f;
+
+    void Update()
+    {
+        // Move tile downwards every frame
+        transform.Translate(Vector3.down * speed * Time.deltaTime);
+    }
+
+    void OnMouseDown()
+    {
+        // Called when user clicks/taps the tile
+        // TODO: later we'll call GameManager.Instance.AddPoint()
+        Destroy(gameObject);
+    }
+
+    void OnBecameInvisible()
+    {
+        // If tile moves off-screen (bottom), destroy to avoid memory leak
+        Destroy(gameObject);
+    }
+}
